bd58a5fbfc6b5519b0f3049fb20d5a8370536e03
Matthew Ruttley <mruttley@mozilla.com>
2014-09-02 21:44:02 -0400

    further work - various tfidf methods. Tried to compress to a single tfidf function but the datasets are too different. To speed things up, the payload should be log'd tfidf already (or maybe as much of the cosine as possible). Have to work out the cosine similarity implementation next and also accomodate for multipart history item triplets.

--- lwca.js
+++ lwca.js
@@ -181,39 +181,191 @@ function remove_persistent_title_components(sessions){
 	return sessions
 }
 
+function tfidf_matrix(data, counts=false) {
+	//Accepts a list of strings or list of count objects
+	//Returns a list of objects where each object's keys are assigned scores according to TFIDF
+	
+	let document_frequencies = {}
+	let basic_term_frequency_vectors = {}
+	
+	//basic frequencies
+	if (counts == false) {
+		for(let document of data){
+			word_frequencies = {}
+			for (let word of document.split(" ")) { //need better tokenization
+				if (word_frequencies.hasOwnProperty(word)) {
+					word_frequencies[word] += 1
+				}else{
+					word_frequencies[word] = 1
+				}
+			}
+			basic_term_frequency_vectors.push(word_frequencies)
+		}
+	}else{
+		basic_term_frequency_vectors = data 
+		data = 0 //clear memory
+	}
+	
+	//document counts
+	for (let v of basic_term_frequency_vectors) {
+		for (let word in v) {
+			if (document_frequencies.hasOwnProperty(word)) {
+				document_frequencies[word] += 1
+			}else{
+				document_frequencies[word] = 1
+			}
+		}
+	}
+	
+	//idf for each word
+	let N = data.length
+	let idf = {}
+	for (let word in document_frequencies) {
+		idf[word] = Math.log(N/document_frequencies[word])
+	}
+	
+	//now final scores for each vector
+	tfidf_vectors = []
+	while(basic_term_frequency_vectors.length > 0){
+		let v = basic_term_frequency_vectors.shift() //pop index 0
+		for (let word in v) {
+			tf = 1 + Math.log(v[word])
+			idf = idf[word]
+			v[word] = tf * idf
+		}
+		tfidf_vectors.push(v)
+	}
+	return tfidf_vectors
+}
+
 function classifySessions(){
 	//Main handler function
 	//Returns an array of arrays [[], [], ...]
 	//Each sub-array like: [start_id, end_id, classification]
 	
 	console.log("Getting user data...")
-	sessions = getHistory()
+	let sessions = getHistory()
 	console.log("Cleaning page titles of persistent components...")
-	sessions = remove_persistent_title_components_across_sessions(sessions)
+	let sessions = remove_persistent_title_components_across_sessions(sessions)
 	
 	console.log("Reading in wordlist")
 	const fileIO = require("sdk/io/file")
 	let dict = fileIO.read("dict_no_stopwords.txt")
-	let words = {} //interpreted as an object as lookups are O(1) rather than O(n)
+	let stopwords = {} //interpreted as an object as lookups are O(1) rather than O(n)
 	for (let word in dict.split("\n")) {
-		words[word] = 1
+		stopwords[word] = 1
 	}
 	
-	console.log("Reading in category payload and creating vectors")
+	console.log("Reading in category payload and creating tfidf_matrix per category")
 	let payloadfile = fileIO.read("payload.lwca").split("\n")
+	
 	category_vectors = {}
+	df = {}
 	for (let line of payloadfile) {
-		line = line.split("\t")
+		let line = line.split("\t")
 		category = line[0]
-		counts = {}
-		for (x=1;x<line.length;x+=2) {
-			counts[line[x]] = parseInt(line[x+1])
+		tf = {}
+		
+		for (x=1;x<line.length;x+=2) { //process logs of each count
+			tf[line[x]] = 1 + Math.log(parseInt(line[x+1]))
 		}
-		category_vectors[category] = counts
+		
+		for (let t in tf){  //process document frequencies
+			if (df.hasOwnProperty(t)) {
+				df[t] += 1
+			}else{
+				df[t] = 1
+			}
+		}
+		category_vectors[category] = tf //add to main object
 	}
 	
+	let N = category_vectors.length
+	let idf = {} //calculate IDF for each word
+	for (let word in df) {
+		idf[word] = Math.log(N/df[word])
+	}
+	df = 0 //clear memory
+	
+	let category_tfidf = {}
+	for (let category in category_vectors) {
+		let vector = category_vectors[category]
+		for (let word in vector) {
+			let tf = vector[word]
+			let word_idf = idf[word]
+			vector[word] = tf * word_idf
+		}
+		category_tfidf[category] = vector
+	}
+	
+	category_vectors = 0 //clear memory
+	idf = 0
+	
+	//now calculate tfidf for sessions
+	console.log("converting sessions to tfidf")
+	
+	session_tf = {}
+	df = {}
+	for (let session_number of sessions) {
+		let session = sessions[session_number]
+		let tf = {}
+		for (let word in session.split(" ")) { // TODO: better tokenization than this
+			if (tf.hasOwnProperty(word)) {
+				tf[word] += 1
+			}else{
+				tf[word] = 1
+			}
+		}
+		session_tf["session_" + session_number] = tf
+		for (let word in tf){
+			if (df.hasOwnProperty(word)) {
+				df[word] += 1
+			}else{
+				df[word] = 1
+			}
+		}
+	}
+	//now get idf
+	let N = sessions.length
+	let idf = {} //calculate IDF for each word
+	for (let word in df) {
+		idf[word] = Math.log(N/df[word])
+	}
+	df = 0 //clear memory
+	
+	//calculate final tfidf
+	let session_tfidf = {}
+	for (let session in session_tf) {
+		let vector = session_tf[session]
+		for (let word in vector) {
+			let tf = vector[word]
+			let word_idf = idf[word]
+			vector[word] = tf * word_idf
+		}
+		session_tfidf[session] = vector
+	}
+	
+	session_tf = 0 //clear memory
+	idf = 0
+	
+	//now we have two tfidf matrices, session_tfidf and category_tfidf
+	//so classify the sessions using the cosine similarity
+	
+	results = {}
+	
+	for (let session in session_tfidf) {
+		let vector = session_tfidf[session]
+		for (let category in category_tfidf) {
+			cosine_similarity = 
+		}
+	}
 	
 }
+
+
+
+
+
 
 
 //def classify_sessions_to_top_level():
